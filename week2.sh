ac756890d3db2efaefc8722396a86d37d7b7c8b4
Evander Ypey <evander_ypey@hotmail.com>
2018-05-25 14:31:13 +0200

    Added functions

--- week2.sh
+++ week2.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 #Script voor soorteren van foto's
 clear;
-directory="";
-sort="";
-while [ -z "$directory" ]; do
-	read -p "Geef de directory op waar het bestand staat (bijv. /home/administrator/imgs): " directory;
-	if [ -z "$directory" ]; then
-		echo "Er moet een directory worden opgegeven!";
-	fi
-done
-while [ -z "$sort" ]; do
-	read -p "Sorteren op week of maand? :" sort
-	if [ -z "$sort" ]; then
-		echo "Er moet een keuze gemaakt worden tussen week of maand!";
-	fi
-done
+get_sourcedest() {
+	directory="";
+	while [ -z "$directory" ]; do
+		read -p "Geef de directory op waar het bestand staat (bijv. /home/administrator/imgs): " directory;
+		if [ -z "$directory" ]; then
+			echo "Er moet een directory worden opgegeven!";
+		fi
+	done
+	echo $directory;
+}
+get_sort() {
+	sort="";
+	while [ -z "$sort" ]; do
+		read -p "Sorteren op week of maand? :" sort
+		if [ -z "$sort" ]; then
+			echo "Er moet een keuze gemaakt worden tussen week of maand!";
+		fi
+	done
+	echo $sort;
+}
+directory=$(get_sourcedest);
+sort=$(get_sort);
 destdir=/home/administrator/images;
 wdate=$(date +%V);
 mdate=$(date +%m);
