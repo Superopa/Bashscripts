4262a01e9fc6d21276238ced3585f63c5ccde0fa
Superopa <evander_ypey@hotmail.com>
2018-06-28 10:53:04 +0200

    Add files via upload

---
+++ installSyslogNGServer.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Script voor het installeren van een Nagios Server
+echo "Dit script installeert syslog-ng en maakt hiervan een server.";
+sudo apt-get install syslog-ng;
+echo "Maakt een back-up van de standaard config file.";
+sudo mv /etc/syslog-ng/syslog-ng.conf /etc/syslog/syslog-ng.conf.BAK;
+echo "Maakt nu een nieuwe config file.";
+echo "
+@version: 3.5
+@include "scl.conf"
+@include "`scl-root`/system/tty10.conf"
+    options {
+        time-reap(30);
+        mark-freq(10);
+        keep-hostname(yes);
+        };
+    source s_local { system(); internal(); };
+    source s_network {
+        syslog(transport(tcp) port(514));
+        };
+    destination d_local {
+    file("/var/log/syslog-ng/messages_${HOST}"); };
+    destination d_logs {
+        file(
+            "/var/log/syslog-ng/logs.txt"
+            owner("root")
+            group("root")
+            perm(0777)
+            ); };
+    log { source(s_local); source(s_network); destination(d_logs); };" >> /etc/syslog/syslog-ng.conf
+echo "Het bestand 'logs.txt' wordt nu gemaakt in het nieuwe mapje /var/log/syslog-ng.";
+sudo mkdir /var/log/syslog-ng;
+sudo touch /var/log/syslog-ng/logs.txt;
