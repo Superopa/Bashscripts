4c96eb1c6646cd93554cd677520f7f2a193e880d
Superopa <evander_ypey@hotmail.com>
2018-06-29 09:44:49 +0200

    Dynamic ip

--- installSalt.bash
+++ installSalt.bash
@@ -5,7 +5,8 @@ echo "Dit script zorgt ervoor dat deze machine een Minion wordt";
 echo "Downloaden van saltstack install bestand...";
 curl -L https://bootstrap.saltstack.com -o install_salt.sh;
 echo "Installeren van saltstack...";
-sudo sh install_salt.sh -A 10.0.0.4
+read -p "Wat is het ip adres van de master?" ip;
+sudo sh install_salt.sh -A $ip;
 echo "Installatie is voltooid. Herstarten van de saltstack-minion service..."
 sudo systemctl restart salt-minion
 echo "Restart succesvol. Het kan 5 minuten duren voordat de minion zich aanbied bij de master.";
