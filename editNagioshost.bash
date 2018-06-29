70fc3cf8b136e1ca162fd4c73dced21be1b863a7
Superopa <evander_ypey@hotmail.com>
2018-06-29 10:11:51 +0200

    Update editNagioshost.bash

--- editNagioshost.bash
+++ editNagioshost.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script voor het configureren van de Nagios server om een host toe te voegen.
 echo "Dit script zorgt ervoor dat de server de client kan accepteren.";
-read -p "Geef een hostnaam op (Deze is zichtbaar in het Nagios overzicht):  " hostname;
+read -p "Geef een hostnaam op (Deze is zichtbaar in het Nagios overzicht): " hostname;
 read -p "Geef het ip-adres op van de client: " ipaddress;
 echo "Weghalen van het #'je in /usr/local/nagios/etc/server"
 echo "cfg_dir=/usr/local/nagios/etc/servers" >> /usr/local/nagios/etc/nagios.cfg;
@@ -10,7 +10,7 @@ mkdir /usr/local/nagios/etc/servers
 touch /usr/local/nagios/etc/servers/host.cfg
 echo "#Define a host for the remote machine
 define host {
-        use                          linux-host
+        use                          linux-server
         host_name            		 $hostname
         alias                        remotehost
         address                      $ipaddress
